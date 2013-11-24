9ba6a950b12118214232e19b6aa80748b7397aa9
July Antonicheva <july.antonicheva@jcatalog.com>
2013-11-24 14:36:29 +0300

    (WCM-876) Updated to Grails-2.3.3

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,7 +1,7 @@
 hibernate {
     cache.use_second_level_cache=true
     cache.use_query_cache=true // Warning, turning this on causes lock contention
-    cache.provider_class='org.hibernate.cache.EhCacheProvider'
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 
 // environment specific settings
@@ -13,7 +13,7 @@ environments {
             username = "sa"
             password = ""
             dbCreate = 'create-drop' // one of 'create', 'create-drop','update'
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 //            logSql = true
         }
     }
@@ -23,8 +23,19 @@ environments {
             driverClassName = "org.h2.Driver"
             username = "sa"
             password = ""
-            dbCreate = 'update' // one of 'create', 'create-drop','update'
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            properties {
+                maxActive = -1
+                minEvictableIdleTimeMillis=1800000
+                timeBetweenEvictionRunsMillis=1800000
+                numTestsPerEvictionRun=3
+                testOnBorrow=true
+                testWhileIdle=true
+                testOnReturn=false
+                validationQuery="SELECT 1"
+                jdbcInterceptors="ConnectionState"
+            }
 //            logSql = true
         }
     }
@@ -35,7 +46,7 @@ environments {
             username = "sa"
             password = ""
             dbCreate = "create-drop"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
 }
